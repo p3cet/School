c3b79e3ac2bc8bec5a34ca38f786014942b699a2
Przemysław Cicharski <cprzem@gmail.com>
2017-10-12 12:42:08 +0200

    ViewModel

--- SchoolDatabase/Controllers/StudentController.cs
+++ SchoolDatabase/Controllers/StudentController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SchoolDatabase.Models.Database;
 using SchoolDatabase.Controllers.Database;
+using SchoolDatabase.Models.ViewModels;
 
 namespace SchoolDatabase.Controllers
 {
@@ -21,15 +22,34 @@ namespace SchoolDatabase.Controllers
         [HttpGet]
         public ActionResult Add()
         {
+            new Student() { CourseId = 1 };
             return View();
         }
 
-        [HttpPost]
+        [HttpGet]
+        public ActionResult AddWithCourseList()
+        {
+            return View(new StudentViewModel() { courses= Dbc.Course.ToList()});
+        }
+
+        /*[HttpPost]
         public ActionResult Add(Student student)
         {
             if (ModelState.IsValid)
             {
                 Dbc.Student.Add(student);
+                Dbc.SaveChanges();
+            }
+            return RedirectToAction("ShowList");
+        }*/
+
+        [HttpPost]
+        public ActionResult Add(Student studentView)
+        {
+            if (ModelState.IsValid)
+            {
+                //Student student = new Student() { CourseId=studentView.CourseId, FirstName = studentView.FirstName, LastName = studentView.LastName};
+                Dbc.Student.Add(studentView);
                 Dbc.SaveChanges();
             }
             return RedirectToAction("ShowList");
---
+++ SchoolDatabase/Models/ViewModels/StudentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SchoolDatabase.Models.Database;
+namespace SchoolDatabase.Models.ViewModels
+{
+    public class StudentViewModel //: Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CourseId { get; set; }
+        public ICollection<Course> courses { get; set; }
+    }
+}
